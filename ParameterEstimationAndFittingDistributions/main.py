22d1cf3b762897eee7e3d7ed63352951ac31caee
mateidiana <mateidiana10@gmail.com>
2025-03-17 11:23:27 +0200

    modified 4th project

--- ParameterEstimationAndFittingDistributions/main.py
+++ ParameterEstimationAndFittingDistributions/main.py
@@ -14,7 +14,7 @@ ObservedCounts = [1, 6, 11, 28, 56, 105, 126, 146, 164, 161, 123, 101, 74, 53, 2
 
 # Plotting the observed data
 plt.figure(figsize=(10, 6))
-plt.plot(ClicksPerTenSeconds, ObservedCounts, marker='o', linestyle='-', color='blue')
+plt.plot(ClicksPerTenSeconds, ObservedCounts, marker='o', linestyle='-', color='blue', label='Observed data')
 
 # Calculate the sample mean
 sample_mean = np.average(ClicksPerTenSeconds, weights=ObservedCounts)
@@ -23,13 +23,15 @@ sample_mean = np.average(ClicksPerTenSeconds, weights=ObservedCounts)
 poisson_dist = poisson.pmf(ClicksPerTenSeconds, mu=sample_mean) * np.sum(ObservedCounts)
 
 # Plotting the poisson distribution
-plt.plot(ClicksPerTenSeconds, poisson_dist, marker='x', linestyle='--', color='red')
+plt.plot(ClicksPerTenSeconds, poisson_dist, marker='x', linestyle='--', color='red', label='Poisson distribution with '
+                                                                                           f'lambda={sample_mean:.2f}')
 
 # Adding labels and title
 plt.title('Observed Alpha Particle Emissions per 10-second interval', fontsize=14)
 plt.xlabel('Number of Clicks per 10-second interval', fontsize=12)
 plt.ylabel('Observed count', fontsize=12)
 plt.grid(True)
+plt.legend()
 
 # Set x-axis to display integer tick marks only
 plt.xticks(ticks=np.arange(0, max(ClicksPerTenSeconds)+1, 1))
